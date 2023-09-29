5f99c426d883e089e8dbeda94352b99ba53e609f
Ed Gonzales <edgonzales6@gmail.com>
2023-09-29 15:51:43 -0700

    Lab 1 completed

--- js/app.js
+++ js/app.js
@@ -7,38 +7,30 @@ const menuLinks = [
 
 const mainEl = document.querySelector('main');
 localStorage['myMainElKey'] = JSON.stringify(mainEl);
-console.log(localStorage);
 mainEl.style.background = ('var(--main-bg)');
 mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
 mainEl.classList.add('flex-ctr');
 const topMenuEl = document.getElementById('top-menu');
-console.log(topMenuEl);
 localStorage['myTopMenuElKey'] = JSON.stringify(topMenuEl)
-console.log(localStorage);
 topMenuEl.style.height = '100%';
-console.log(topMenuEl.style.height);
 topMenuEl.style.background = 'var(--top-menu-bg)';
 topMenuEl.classList.add('flex-around');
 
 /*
 Task 3.1
 Iterate over the entire menuLinks array and for each "link" object:
-
 Create an <a> element.
-
 On the new element, add an href attribute with its value set to the 
 href property of the "link" object.
-
 Set the new element's content to the value of the text property of 
 the "link" object.
-
 Append the new element to the topMenuEl element.
 */
 menuLinks.forEach(function(link){
     const aEl = document.createElement('a');
     aEl.setAttribute('href', link.href);
     //console.log(aEl);
-    aEl.textContent = menuLinks.text;
+    aEl.textContent = link.text;
     topMenuEl.appendChild(aEl);
-    
+    console.log(aEl.textContent);
 })
